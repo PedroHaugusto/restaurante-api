de4089f22be010df2fa852494d737d3d04022b09
Pedro Henrique <pedrohaugusto12@gmail.com>
2024-11-22 11:28:52 -0300

    :wrench: chore: Iniciando projeto

---
+++ restauranteapi/src/main/java/com/restaraunteapi/restauranteapi/RestauranteapiApplication.java
@@ -0,0 +1,13 @@
+package com.restaraunteapi.restauranteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteapiApplication.class, args);
+	}
+
+}
